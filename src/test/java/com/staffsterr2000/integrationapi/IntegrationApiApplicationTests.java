a2b81ada618150515e42ddf7fcefcad02cc150f1
staffsterr2000 <rosenkostas@gmail.com>
2022-01-18 13:12:29 +0200

    Spring initializer

---
+++ src/test/java/com/staffsterr2000/integrationapi/IntegrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staffsterr2000.integrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
